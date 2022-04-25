6e1470e8dc4cb42d76d1c3804a339ab0982ee9b8
Geraldine <geraldine99>
2022-04-25 17:00:44 +0200

    map

--- grid1.1.py
+++ grid1.1.py
@@ -45,7 +45,34 @@ class constructor(cell):
         for typ in self.type:
             self.type_construct = random.choice(typ)  #ou dans cell?
 
-
+#child class
+class groomed(cell):
+    def __init__(self, row, column, type, dimension, pente):
+        cell.__init__(self, row, column, type, dimension, pente)
+        self.fall = 0.15
+class powder(cell):
+    def __init__(self, row, column, type, dimension, pente):
+        cell.__init__(self, row, column, type, dimension, pente)
+        self.fall = 0.10
+class ice(cell):
+    def __init__(self, row, column, type, dimension, pente):
+        cell.__init__(self, row, column, type, dimension, pente)
+        self.fall = 0.6
+class grass(cell):
+    def __init__(self, row, column, type, dimension, pente):
+        cell.__init__(self, row, column, type, dimension, pente)
+        self.fall = 0.95
+class bumps(cell):
+    def __init__(self, row, column, type, dimension, pente):
+        cell.__init__(self, row, column, type, dimension, pente)
+        self.fall = 0.4
+        #if speed > x: self.fall= 0.8
+        #if speed < x: self.fall= 0.2
+class trees(cell):
+    def __init__(self, row, column, type, dimension, pente):
+        cell.__init__(self, row, column, type, dimension, pente)
+        self.fall = 1
+        #if trees: break
 
 
 
